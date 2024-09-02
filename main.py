f37ebbc79c04b9a476b61470affc51a7d4c77d06
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-02 20:45:07 +0200

    trying out Apache Libcloud to manage Exoscale compute instances

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from libcloud.compute.types import Provider
+from libcloud.compute.providers import get_driver
+
+
+if __name__ == '__main__':
+    username = os.getenv('CLOUD_USERNAME')
+    api_key = os.getenv('CLOUD_API_KEY')
+    if not username or not api_key:
+        print('must provide CLOUD_USERNAME and CLOUD_API_KEY', file=sys.stderr)
+        sys.exit(1)
+
+    cls = get_driver(Provider.EXOSCALE)
+    driver = cls(username, api_key)
+    print(driver)
