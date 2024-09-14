d95f4735f8a119873b5c831481a77e4b79360f6b
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-14 19:38:50 +0200

    use dotenv for configuration

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 import os
 import sys
 
+from dotenv import load_dotenv
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 
 
 if __name__ == '__main__':
+    load_dotenv()
     username = os.getenv('CLOUD_USERNAME')
     api_key = os.getenv('CLOUD_API_KEY')
     if not username or not api_key:
